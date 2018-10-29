f08dd9156bc7e10bed63669a1bdacbfb5519d98a
yongzhenghere <happyguyszyz@gmail.com>
2018-10-29 20:38:53 +0300

    fix

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -1,103 +1,82 @@
 package lesson1;
 
 import kotlin.NotImplementedError;
-
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-
 
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС,
      * каждый на отдельной строке. Пример:
-     *
+     * <p>
      * 13:15:19
      * 07:26:57
      * 10:00:03
      * 19:56:14
      * 13:15:19
      * 00:40:31
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 00:40:31
      * 07:26:57
      * 10:00:03
      * 13:15:19
      * 13:15:19
      * 19:56:14
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public void sortTimes(String inputName, String outputName) throws IOException, ParseException {
-        List<Date> dateList = new ArrayList<>();
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                Date date = formatter.parse(line);
-                dateList.add(date);
-            }
-        }
-        Collections.sort(dateList);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outputName));
-        for (Date element : dateList) {
-            writer.write(formatter.format(element));
-            writer.newLine();
-        }
-        writer.close();
+    static public void sortTimes(String inputName, String outputName) {
+        throw new NotImplementedError();
     }
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public void sortAddresses(String inputName, String outputName) {
+    static public void sortAddresses(String inputName, String outputName) throws IOException {
         throw new NotImplementedError();
     }
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -105,11 +84,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -118,68 +97,18 @@ public class JavaTasks {
      * 99.5
      * 121.3
      */
-    static public void sortTemperatures(String inputName, String outputName) throws IOException {
-        List<Double> tempList = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                Double temp = Double.parseDouble(line);
-                if (temp >= -273 && temp <= 500) {
-                 tempList.add(temp);
-                }
-            }
-        }
-        quickSort(tempList);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outputName));
-        for (Double element : tempList) {
-            writer.write(Double.toString(element));
-            writer.newLine();
-        }
-        writer.close();
-    }
-    static final Random random = new Random(Calendar.getInstance().getTimeInMillis());
-
-    private static int partition(List<Double> elements, int min, int max) {
-        double x = elements.get(min + random.nextInt(max - min + 1));
-        int left = min, right = max;
-        while (left <= right) {
-            while (elements.get(left) < x) {
-                left++;
-            }
-            while (elements.get(right) > x) {
-                right--;
-            }
-            if (left <= right) {
-                Double temp = elements.get(left);
-                elements.set(left, elements.get(right));
-                elements.set(right, temp);
-                left++;
-                right--;
-            }
-        }
-        return right;
-    }
-
-    private static void quickSort(List<Double> elements, int min, int max) {
-        if (min < max) {
-            int border = partition(elements, min, max);
-            quickSort(elements, min, border);
-            quickSort(elements, border + 1, max);
-        }
-    }
-
-    public static void quickSort(List<Double> elements) {
-        quickSort(elements, 0, elements.size() - 1);
+    static public void sortTemperatures(String inputName, String outputName) {
+        throw new NotImplementedError();
     }
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -187,12 +116,12 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
@@ -201,34 +130,25 @@ public class JavaTasks {
      * 2
      * 2
      */
-    static public void sortSequence(String inputName, String outputName) throws IOException, IllegalFormatException {
-
+    static public void sortSequence(String inputName, String outputName) {
+        throw new NotImplementedError();
     }
+
     /**
      * Соединить два отсортированных массива в один
-     *
+     * <p>
      * Простая
-     *
+     * <p>
      * Задан отсортированный массив first и второй массив second,
      * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
      * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
+     * <p>
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
-     *
+     * <p>
      * Результат: second = [1 3 4 9 9 13 15 20 23 28]
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
-        List<T> list = new ArrayList<>();
-        System.arraycopy(first, 0, second, 0, first.length);
-        for (int i = 0; i < second.length; i++) {
-            if (second[i] != null) {
-                list.add(second[i]);
-            }
-        }
-        for (int i = 0; i < list.size() - 1; i++) {
-            second[i] = list.get(i);
-        }
-        Arrays.sort(second);
+        throw new NotImplementedError();
     }
 }
